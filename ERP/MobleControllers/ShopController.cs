82be6d4d0ae85b38a5d684fc9e97cc0dff37c68e
wangweicoder <495577207@qq.com>
2018-03-02 11:29:54 +0800

    购物车和详情页单买，跳转确认支付页面

--- ERP/MobleControllers/ShopController.cs
+++ ERP/MobleControllers/ShopController.cs
@@ -31,7 +31,86 @@ namespace ERP.MobleControllers
         [HttpGet]
         public ActionResult PayOrders(string goodnum, string FlowerNums)
         {
+            string ids = Request["ids"].TrimEnd(',');
+            ViewData["ids"] = ids;
+            //ViewData["PayTotal"] = Orders.SellingPrice;
+            ViewData["msg"] = " 提示：请在30分钟内完成在线支付，逾期将视为订单无效";           
+            Business.Sys_FlowerShopCart Sys_OrdersManaage = new Business.Sys_FlowerShopCart();
+            StringBuilder sb = new StringBuilder();
+            if (!string.IsNullOrEmpty(userid))
+            {
+                sb.Append(" and UsersId='" + userid + "'");
+            }
+            sb.Append(" and  b.FlowerId in("+ids+")");
+            List<Model.FlowerCartVM> list = Sys_OrdersManaage.FlowerShopCartList(sb.ToString());
+            Model.CartLine model = new Model.CartLine();
+            model.Products = list;
+            return View(model);
+            //return Redirect("/WxPay/Index?OrdersId=" + Orders.OrderId + "&PayTotal=" + Flower.FlowerSalesPrice * int.Parse(OrdersNum));
+            
+        }
+        [HttpPost]
+        public ActionResult PayOrders(string id)
+        {
+            #region 生成订单
+            ////string id = Request["id"];
+            //Business.Sys_Flower Sys_Flower = new Business.Sys_Flower();
+            //Model.Flower Flower = Sys_Flower.GetFlower(id);
+            //string FlowerNum = Request["FlowerNum"];
+            //string address = Request["province"] + Request["city"] + Request["area"] + Request["ConsigneAaddress"];
+            //string ConsigneeName = Request["ConsigneeName"];
+            //string ConsigneePhone = Request["ConsigneePhone"];
+            //Model.Orders Orders = new Model.Orders();
+            //Orders.UsersId = Utility.ChangeText.GetUsersId();
+            //Orders.CreateTime = DateTime.Now;
+            //Orders.SellingPrice = Flower.FlowerSalesPrice * int.Parse(FlowerNum);
+            //Orders.OrdersState = 1;
+            //Orders.OrderId = Utility.ChangeText.OrderIdCreate();
+            //Orders.GoodsSum = int.Parse(FlowerNum);
+            //Orders.CostPrice = Flower.FlowerCostPrice * int.Parse(FlowerNum);
+            //Orders.ConsigneeName = ConsigneeName;
+            //Orders.ConsigneePhone = ConsigneePhone;
+            //Orders.ConsigneAaddress = address;
+            //List<Model.OrdersDetails> OrdersDetailsList = new List<Model.OrdersDetails>();
+            //for (int i = 0; i < int.Parse(FlowerNum); i++)
+            //{
+            //    Model.OrdersDetails OrdersDetails = new Model.OrdersDetails();
+            //    OrdersDetails.OrderId = Orders.OrderId;
+            //    OrdersDetails.FlowerNumber = Flower.FlowerNumber;
+            //    OrdersDetails.FlowerWatchName = Flower.FlowerWatchName;
+            //    OrdersDetails.FlowerWatchPhoto = Flower.FlowerWatchPhoto;
+            //    OrdersDetails.SellingPrice = Flower.FlowerSalesPrice;
+            //    OrdersDetails.SellingNum = 1;//从原来的FlowerNum修改成1
+            //    OrdersDetails.CostPrice = Flower.FlowerCostPrice;
+            //    OrdersDetailsList.Add(OrdersDetails);
+            //}
+            //Model.OrdersLog OrdersLog = new Model.OrdersLog();
+            //OrdersLog.OrdersId = Orders.OrderId;
+            //OrdersLog.OrdersState = 1;
+            //OrdersLog.UserName = Utility.ChangeText.GetUserName();
+            //OrdersLog.Remark = "商品详情页中下单";
+            //OrdersLog.Time = DateTime.Now;
 
+            //Business.Sys_OrdersManaage Sys_OrdersManaage = new Business.Sys_OrdersManaage();
+            //Sys_OrdersManaage.InsertOrders(Orders, OrdersDetailsList, OrdersLog);
+           #endregion           
+            ViewData["ids"] = id;
+            ViewData["msg"] = " 提示：请在30分钟内完成在线支付，逾期将视为订单无效"; 
+            Business.Sys_Flower Sys_Flower = new Business.Sys_Flower();
+            Model.Flower flower= Sys_Flower.GetFlower(id);
+            Model.FlowerCartVM fc=new Model.FlowerCartVM();
+            fc.Id=0;
+            fc.FlowerId=flower.id.ToString();
+            fc.Num = int.Parse(Request["FlowerNum"]);
+            fc.FlowerSalesPrice=flower.FlowerSalesPrice;
+            fc.FlowerWatchPhoto = flower.FlowerWatchPhoto;
+            List<Model.FlowerCartVM> list = new List<Model.FlowerCartVM> { fc};
+            Model.CartLine model = new Model.CartLine();
+            model.Products = list;
+            return View(model);            
+        }
+        public ActionResult PayOrdersNow(string Remark)
+        {
             #region 生成订单
             string ids = Request["ids"].TrimEnd(',');
             Business.Sys_Flower Sys_Flower = new Business.Sys_Flower();
@@ -39,9 +118,10 @@ namespace ERP.MobleControllers
             string[] idarry = ids.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
             string FlowerNum = Request["FlowerNums"];
             string[] FlowerNumarry = FlowerNum.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
-            string address = Request["province"] + Request["city"] + Request["area"] + Request["ConsigneAaddress"];
-            string ConsigneeName = "";
-            string ConsigneePhone = "";
+            
+            string ConsigneAaddress =  Request["DetailedAddress"];
+            string ConsigneeName = Request["ConsigneeName"];
+            string ConsigneePhone = Request["ConsigneePhone"];
             //唯一的订单
             Model.Orders Orders = new Model.Orders();
             Orders.UsersId = int.Parse(userid);
@@ -51,7 +131,7 @@ namespace ERP.MobleControllers
             Orders.GoodsSum = int.Parse(Request["goodnum"]);// 总件数
             Orders.ConsigneeName = ConsigneeName;
             Orders.ConsigneePhone = ConsigneePhone;
-            Orders.ConsigneAaddress = address;
+            Orders.ConsigneAaddress = ConsigneAaddress;
             //详情列表
             List<Model.OrdersDetails> OrdersDetailsList = new List<Model.OrdersDetails>();
             for (int j = 0; j < idarry.Length; j++)
@@ -79,73 +159,13 @@ namespace ERP.MobleControllers
             OrdersLog.OrdersId = Orders.OrderId;
             OrdersLog.OrdersState = 1;
             OrdersLog.UserName = Utility.ChangeText.GetUserName();
-            OrdersLog.Remark = "购物车中下单";
+            OrdersLog.Remark = Remark;
             OrdersLog.Time = DateTime.Now;
 
             #endregion
             Sys_OrdersManaage.TransactionAddOrders(Orders, OrdersDetailsList, OrdersLog);
-            ViewData["OrdersId"] = Orders.OrderId;
-            ViewData["PayTotal"] = Orders.SellingPrice;
-            ViewData["msg"] = " 提示：请在30分钟内完成在线支付，逾期将视为订单无效";
-
-            //return Redirect("/WxPay/Index?OrdersId=" + Orders.OrderId + "&PayTotal=" + Flower.FlowerSalesPrice * int.Parse(OrdersNum));
-            return View();
-        }
-        [HttpPost]
-        public ActionResult PayOrders(string id)
-        {
-            #region 生成订单
-            //string id = Request["id"];
-            Business.Sys_Flower Sys_Flower = new Business.Sys_Flower();
-            Model.Flower Flower = Sys_Flower.GetFlower(id);
-            string FlowerNum = Request["FlowerNum"];
-            string address = Request["province"] + Request["city"] + Request["area"] + Request["ConsigneAaddress"];
-            string ConsigneeName = Request["ConsigneeName"];
-            string ConsigneePhone = Request["ConsigneePhone"];
-            Model.Orders Orders = new Model.Orders();
-            Orders.UsersId = Utility.ChangeText.GetUsersId();
-            Orders.CreateTime = DateTime.Now;
-            Orders.SellingPrice = Flower.FlowerSalesPrice * int.Parse(FlowerNum);
-            Orders.OrdersState = 1;
-            Orders.OrderId = Utility.ChangeText.OrderIdCreate();
-            Orders.GoodsSum = int.Parse(FlowerNum);
-            Orders.CostPrice = Flower.FlowerCostPrice * int.Parse(FlowerNum);
-            Orders.ConsigneeName = ConsigneeName;
-            Orders.ConsigneePhone = ConsigneePhone;
-            Orders.ConsigneAaddress = address;
-            List<Model.OrdersDetails> OrdersDetailsList = new List<Model.OrdersDetails>();
-            for (int i = 0; i < int.Parse(FlowerNum); i++)
-            {
-                Model.OrdersDetails OrdersDetails = new Model.OrdersDetails();
-                OrdersDetails.OrderId = Orders.OrderId;
-                OrdersDetails.FlowerNumber = Flower.FlowerNumber;
-                OrdersDetails.FlowerWatchName = Flower.FlowerWatchName;
-                OrdersDetails.FlowerWatchPhoto = Flower.FlowerWatchPhoto;
-                OrdersDetails.SellingPrice = Flower.FlowerSalesPrice;
-                OrdersDetails.SellingNum = 1;//从原来的FlowerNum修改成1
-                OrdersDetails.CostPrice = Flower.FlowerCostPrice;
-                OrdersDetailsList.Add(OrdersDetails);
-            }
-            Model.OrdersLog OrdersLog = new Model.OrdersLog();
-            OrdersLog.OrdersId = Orders.OrderId;
-            OrdersLog.OrdersState = 1;
-            OrdersLog.UserName = Utility.ChangeText.GetUserName();
-            OrdersLog.Remark = "商品详情页中下单";
-            OrdersLog.Time = DateTime.Now;
-
-            Business.Sys_OrdersManaage Sys_OrdersManaage = new Business.Sys_OrdersManaage();
-            Sys_OrdersManaage.InsertOrders(Orders, OrdersDetailsList, OrdersLog);
-            #endregion
-
-            //ViewData["OrdersId"] = Orders.OrderId;
-            //ViewData["PayTotal"] = Orders.SellingPrice;
-            return Redirect("/WxPay/Index?OrdersId=" + Orders.OrderId + "&PayTotal=" + Flower.FlowerSalesPrice * int.Parse(FlowerNum));
-            //return View();
-        }
-        public ActionResult PayOrdersNow()
-        {           
-           AddInfo();
-           return Redirect("/WxPay/Index?OrdersId=" + Request["OrdersId"] + "&PayTotal=" + Request["PayTotal"]);
+            //
+            return Redirect("/WxPay/Index?OrdersId=" + Orders.OrderId + "&PayTotal=" + Orders.SellingPrice);
           
         }
 
