788b4182a4165a222bb9170b29cb38940fa09acf
wangweicoder <495577207@qq.com>
2019-11-04 21:22:31 +0800

    新用户跳到登录页

--- ERP/MobleControllers/WxHelperController.cs
+++ ERP/MobleControllers/WxHelperController.cs
@@ -57,62 +57,63 @@ namespace ERP.MobleControllers
                 }
                 else if (Request["way"] == "Arrangement" )
                 {
-                    string gzhurl = "https://mp.weixin.qq.com/mp/profile_ext?action=home&__biz=" + System.Configuration.ConfigurationManager.AppSettings["WxAppId"] + "&scene=110#wechat_redirect";
-                    string result = Utility.PostData.GetData(gzhurl);
-                    Utility.Log.WriteTextLog("微信自动关注公众号", "result", result, "gzhurl", gzhurl);
+                    //string gzhurl = "https://mp.weixin.qq.com/mp/profile_ext?action=home&__biz=" + System.Configuration.ConfigurationManager.AppSettings["WxAppId"] + "&scene=110#wechat_redirect";
+                    //string result = Utility.PostData.GetData(gzhurl);
+                    //Utility.Log.WriteTextLog("微信自动关注公众号", "result", result, "gzhurl", gzhurl);
                     if (UserAdmin == null )
                     {
-                        System.Random Random = new System.Random();
-                        int Result = Random.Next(0, 9999);
-                        Business.Sys_Role Sys_Role = new Business.Sys_Role();
-                        Model.RoleInfo RoleInfo = Sys_Role.GetRoleInfoByRoleCode("Tourist");
-                        //写入一条记录 标识为游客
-                        Model.UserAdmin UserAdminTourist = new Model.UserAdmin();
-                        UserAdminTourist.UserName = "WeiXin_" + Result.ToString() + DateTime.Now.ToString("yyyy-MM-dd");
-                        UserAdminTourist.PassWord = Utility.ChangeText.md5("123456");
-                        UserAdminTourist.IsEnable = 0;
-                        UserAdminTourist.OpenId = OpenId;
-                        UserAdminTourist.RoleCode = RoleInfo.RoleCode;
-                        UserAdminTourist.RoleName = RoleInfo.RoleName;
-                        UserAdminTourist.RealName = "游客未知";
-                        Session["UsersId"] = Sys_UserAdmin.InsertUserAdminGetId(UserAdminTourist);
-                        Session["UserName"] = UserAdminTourist.UserName;
-                        Session["RealName"] = UserAdminTourist.RealName;
-                        Session["RoleCode"] = UserAdminTourist.RoleCode;
-                        //记录登录日志 
-                        Business.Sys_UsersLoginLog Sys_Userlog = new Business.Sys_UsersLoginLog();
-                        Model.UsersLoginLog model = new Model.UsersLoginLog();
-                        model.UsersId = UserAdmin.ID.ToString();
-                        model.UserName = UserAdmin.UserName;
-                        model.RealName = UserAdmin.RealName;
-                        model.LoginTime = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
-                        System.DateTime currentTime = DateTime.Now;
-                        model.Year = currentTime.Year.ToString();
-                        model.Month = currentTime.Month.ToString();
-                        model.Day = currentTime.Day.ToString();
-                        Sys_Userlog.InsertUsersLoginLog(model);
-                        return RedirectToAction("GetArrangementInfo", "MMain", new { ArrangementId = Request["id"]});
+                        //System.Random Random = new System.Random();
+                        //int Result = Random.Next(0, 9999);
+                        //Business.Sys_Role Sys_Role = new Business.Sys_Role();
+                        //Model.RoleInfo RoleInfo = Sys_Role.GetRoleInfoByRoleCode("Tourist");
+                        ////写入一条记录 标识为游客
+                        //Model.UserAdmin UserAdminTourist = new Model.UserAdmin();
+                        //UserAdminTourist.UserName = "WeiXin_" + Result.ToString() + DateTime.Now.ToString("yyyy-MM-dd");
+                        //UserAdminTourist.PassWord = Utility.ChangeText.md5("123456");
+                        //UserAdminTourist.IsEnable = 0;
+                        //UserAdminTourist.OpenId = OpenId;
+                        //UserAdminTourist.RoleCode = RoleInfo.RoleCode;
+                        //UserAdminTourist.RoleName = RoleInfo.RoleName;
+                        //UserAdminTourist.RealName = "游客未知";
+                        //Session["UsersId"] = Sys_UserAdmin.InsertUserAdminGetId(UserAdminTourist);
+                        //Session["UserName"] = UserAdminTourist.UserName;
+                        //Session["RealName"] = UserAdminTourist.RealName;
+                        //Session["RoleCode"] = UserAdminTourist.RoleCode;
+                        ////记录登录日志 
+                        //Business.Sys_UsersLoginLog Sys_Userlog = new Business.Sys_UsersLoginLog();
+                        //Model.UsersLoginLog model = new Model.UsersLoginLog();
+                        //model.UsersId = UserAdmin.ID.ToString();
+                        //model.UserName = UserAdmin.UserName;
+                        //model.RealName = UserAdmin.RealName;
+                        //model.LoginTime = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+                        //System.DateTime currentTime = DateTime.Now;
+                        //model.Year = currentTime.Year.ToString();
+                        //model.Month = currentTime.Month.ToString();
+                        //model.Day = currentTime.Day.ToString();
+                        //Sys_Userlog.InsertUsersLoginLog(model);
+                        //return RedirectToAction("GetArrangementInfo", "MMain", new { ArrangementId = Request["id"]});
+                        return RedirectToAction("Index", "MLogin");
                     }
-                    if (UserAdmin.RoleCode=="Tourist")
-                    {
-                        Session["UsersId"] = UserAdmin.ID;
-                        Session["UserName"] = UserAdmin.UserName;
-                        Session["RealName"] = UserAdmin.RealName; 
-                        Session["RoleCode"] = UserAdmin.RoleCode;
-                        //记录登录日志 
-                        Business.Sys_UsersLoginLog Sys_Userlog = new Business.Sys_UsersLoginLog();
-                        Model.UsersLoginLog model = new Model.UsersLoginLog();
-                        model.UsersId = UserAdmin.ID.ToString();
-                        model.UserName = UserAdmin.UserName;
-                        model.RealName = UserAdmin.RealName;
-                        model.LoginTime = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
-                        System.DateTime currentTime = DateTime.Now;
-                        model.Year = currentTime.Year.ToString();
-                        model.Month = currentTime.Month.ToString();
-                        model.Day = currentTime.Day.ToString();
-                        Sys_Userlog.InsertUsersLoginLog(model);
-                        return RedirectToAction("GetArrangementInfo", "MMain", new { ArrangementId = Request["id"] });
-                    }
+                    //if (UserAdmin.RoleCode=="Tourist")
+                    //{
+                    //    Session["UsersId"] = UserAdmin.ID;
+                    //    Session["UserName"] = UserAdmin.UserName;
+                    //    Session["RealName"] = UserAdmin.RealName; 
+                    //    Session["RoleCode"] = UserAdmin.RoleCode;
+                    //    //记录登录日志 
+                    //    Business.Sys_UsersLoginLog Sys_Userlog = new Business.Sys_UsersLoginLog();
+                    //    Model.UsersLoginLog model = new Model.UsersLoginLog();
+                    //    model.UsersId = UserAdmin.ID.ToString();
+                    //    model.UserName = UserAdmin.UserName;
+                    //    model.RealName = UserAdmin.RealName;
+                    //    model.LoginTime = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+                    //    System.DateTime currentTime = DateTime.Now;
+                    //    model.Year = currentTime.Year.ToString();
+                    //    model.Month = currentTime.Month.ToString();
+                    //    model.Day = currentTime.Day.ToString();
+                    //    Sys_Userlog.InsertUsersLoginLog(model);
+                    //    return RedirectToAction("GetArrangementInfo", "MMain", new { ArrangementId = Request["id"] });
+                    //}
                 }
                 return RedirectToAction("Index", "MLogin"); 
             }
