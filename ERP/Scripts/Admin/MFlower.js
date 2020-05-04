05126512b66bf05fc0fbdac1de3e5b769ec7dcaf
wangweicoder <495577207@qq.com>
2020-05-04 16:48:15 +0800

    0504优化上传服务后图片

--- ERP/Scripts/Admin/MFlower.js
+++ ERP/Scripts/Admin/MFlower.js
@@ -22,36 +22,38 @@
                 "id": id,
                 "ArrangementId": $("#FlowerArrangementId").val()
             });
-        });
-        uploader.on('uploadSuccess', function (file, response) {
-            if (response.msg === "1") {
-                this_.children().remove();
-                var $li = $('<img src=""  class="imgphoto" width="100" height="120" />');
-                $li.attr('src', response.path);
-                this_.html($li);
-            }
-            uploader.reset();
-        });
+        });       
         //
-        uploader.on('fileQueued---', function (file) {
-            this_.children().remove();
-            var $li = $(
-                '<div id="' + file.id + '" class="upload-item">' +
-                '<img >' +               
-                '</div>'
-            ),
-                $img = $li.find('img');
-            this_.html($li);
-            var thumbnailWidth = 100;
-            var thumbnailHeight = 120;
-            // 创建缩略图                    
-            uploader.makeThumb(file, function (error, src) {
-                if (error) {
-                    $img.replaceWith('<span>不能预览</span>');
-                    return;
+        var af = $("#serveraf");
+        if (af.length > 0) {
+            uploader.on('fileQueued', function (file) {
+                this_.children().remove();
+                layer.msg("上传成功");
+                var $img = $('<img >');
+                this_.html($img);
+                var thumbnailWidth = 80;
+                var thumbnailHeight = 80;
+                // 创建缩略图                    
+                uploader.makeThumb(file, function (error, src) {
+                    if (error) {
+                        $img.replaceWith('<span>不能预览</span>');
+                        return;
+                    }
+                    $img.attr('src', src);
+                }, thumbnailWidth, thumbnailHeight);
+            });
+        }
+        else {
+            uploader.on('uploadSuccess', function (file, response) {
+                if (response.msg === "1") {
+                    layer.msg("上传成功");
+                    this_.children().remove();
+                    var $li = $('<img src=""  class="imgphoto" width="100" height="120" />');
+                    $li.attr('src', response.path);
+                    this_.html($li);
                 }
-                $img.attr('src', src);
-            }, thumbnailWidth, thumbnailHeight);
-        });
-    });//end each
+                uploader.reset();
+            });
+        }
+    });//end each  
 }
